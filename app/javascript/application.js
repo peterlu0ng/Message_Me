9a1d088ec625c358f7d28f480215e343d746fce1
Peter Luong <peter.luong678@gmail.com>
2024-05-05 16:12:42 +1000

    allow dropdown functionality and nav partial

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,11 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as jquery from "jquery"
+window.jQuery = jquery
+window.$ = jquery
 import "semantic-ui"
+
+$(document).on('turbo:load', function(){
+    console.log('loaded turbo links')
+    $('.ui.dropdown').dropdown()
+});
