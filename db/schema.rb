ac81aaab0b8e66b58e83a3506f76fa5d4aacca31
Peter Luong <peter.luong678@gmail.com>
2024-05-07 14:28:05 +1000

    generation message model and associations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_041118) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_042224) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
