18aafccf37c999719b018d99c6d8c64d05983e74
Peter Luong <peter.luong678@gmail.com>
2024-05-13 01:07:56 +1000

    create signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :messages
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 15}
   has_secure_password
+
+  validates :username, presence: true,
+              uniqueness: { case_sensitive: false },
+              length: { minimum: 3, maximum: 25 }
 end
