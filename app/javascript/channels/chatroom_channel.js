811644020d6360abca33c5764a84e0fae26c7f0f
Peter Luong <peter.luong678@gmail.com>
2024-05-12 15:48:19 +1000

    modify and broadcast messages

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,5 +11,6 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    alert (data.foo);
   }
 });
