d1998e1a7568d1af1b3c6274c4e81f0a25389f52
Peter Luong <peter.luong678@gmail.com>
2024-05-13 00:17:34 +1000

    fix jquery - dropdown and add autoscroll

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as jquery from "jquery"
-window.jQuery = jquery
-window.$ = jquery
+import "jquery"
 import "semantic-ui"
 import "channels"
 
-$(document).on('turbo:load', function(){
-    console.log('loaded turbo links')
-    $('.ui.dropdown').dropdown()
-});
+$(document).on('turbo:load', function() {
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
+  })
+   
+  // add auto scroll to bottom in messages
+  $(document).on('turbo:load', function() {
+    if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+  })
