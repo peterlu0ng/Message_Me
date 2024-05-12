fdbb82a924771c47815419d821857e8b85326b4a
Peter Luong <peter.luong678@gmail.com>
2024-05-13 00:19:59 +1000

    add autoscroll to new messages and refresh

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,17 +1,18 @@
 import consumer from "channels/consumer"
-
+ 
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log("Connected to chatroom...")
   },
-
+ 
   disconnected() {
     // Called when the subscription has been terminated by the server
   },
-
+ 
   received(data) {
-    var element = document.getElementById("message-container");
-    element.innerHTML += data.mod_message;
-    },
+    // Called when there's incoming data on the websocket for this channel
+    $('#message-container').append(data.mod_message);
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    
+  }
 });
