5220a2f53f0f089150cb7126c6b1597bca2abb71
Peter Luong <peter.luong678@gmail.com>
2024-05-13 02:18:53 +1000

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://mysite-3tes.onrender.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://mysite-3tes.onrender.com/", /http:\/\/mysite-3tes.onrender.com.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
