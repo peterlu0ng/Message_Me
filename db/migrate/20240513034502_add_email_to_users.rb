9123ce1cc4c679948908c0742ad5b7dc5aee4f28
Peter Luong <peter.luong678@gmail.com>
2024-05-13 13:46:19 +1000

    add email to users table

---
+++ db/migrate/20240513034502_add_email_to_users.rb
@@ -0,0 +1,5 @@
+class AddEmailToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_042224) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_034502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_042224) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
